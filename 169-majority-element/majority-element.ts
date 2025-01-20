c9baace1ca6b27c1a2bf2303c57056c25757f733
Artem Petukhov <ptukhovAN@gmail.com>
2025-01-20 17:58:31 +0500

    Time: 0 ms (100.00%) | Memory: 53.7 MB (71.71%) - LeetSync

--- 169-majority-element/majority-element.ts
+++ 169-majority-element/majority-element.ts
@@ -1,20 +1,20 @@
 function majorityElement(nums: number[]): number {
+    let currentMajor = nums[0]; // Инициализируем кандидата
+    let count = 1; // Инициализируем счётчик
 
-    if (nums.length < 2) {
-        return nums[0]
-    }    
-    const majorCount = Math.ceil(nums.length / 2)
-    const map = new Map()
-
-    for (let n of nums) {
-        if (map.has(n)) {
-            const count = (map.get(n) || 0) + 1
-            if (count >= majorCount) {
-                return n
-            }
-            map.set(n, count)
+    for (let i = 1; i < nums.length; i++) {
+        const n = nums[i];
+        if (n === currentMajor) {
+            count += 1; 
         } else {
-            map.set(n, 1)
+            if (count === 0) {
+                currentMajor = n;
+                count = 1;
+            } else {
+                count -= 1; 
+            }
         }
     }
-};
+
+    return currentMajor;
+}
