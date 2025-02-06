adfc9f25b4b2e05924bdd2dc5ebbb8195eca6397
Artem Petukhov <ptukhovAN@gmail.com>
2025-02-06 09:45:16 +0500

    Time: 3 ms (14.52%) | Memory: 52.6 MB (11.08%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.ts
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.ts
@@ -1,49 +1,13 @@
 function searchRange(nums: number[], target: number): number[] {
+    let n = [];
 
-    function findFirst(nums: number[], target: number): number {
-        let left = 0;
-        let right = nums.length - 1;
-        let first = -1;
-
-        while (left <= right) {
-            const middle = Math.floor((left + right) / 2);
-
-            if (nums[middle] === target) {
-                first = middle; 
-                right = middle - 1; 
-            } else if (nums[middle] < target) {
-                left = middle + 1;
-            } else {
-                right = middle - 1;
-            }
+    for (let i = 0; i <= nums.length; i++){
+        if (nums[i] == target){
+            n.push(i)
         }
-
-        return first;
     }
 
-    function findLast(nums: number[], target: number): number {
-        let left = 0;
-        let right = nums.length - 1;
-        let last = -1;
+    if (n.length == 0) return [-1, -1]
 
-        while (left <= right) {
-            const middle = Math.floor((left + right) / 2);
-
-            if (nums[middle] === target) {
-                last = middle; 
-                left = middle + 1; 
-            } else if (nums[middle] < target) {
-                left = middle + 1;
-            } else {
-                right = middle - 1;
-            }
-        }
-
-        return last;
-    }
-
-    const first = findFirst(nums, target);
-    const last = findLast(nums, target);
-
-    return [first, last];
-}
+    return [n[0], n.pop()]
+};
